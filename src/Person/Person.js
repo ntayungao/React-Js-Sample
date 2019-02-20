436956b46de018d2d83a171ee390f0c4d1a50ec6
Niles Kim Ayungao <nileskim.ayungao@gmail.com>
2019-02-21 07:14:32 +0800

    Update Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ import './Person.css';
 
 const person = (props) => {
     return (
-        <div classname="Person">
+        <div className="Person">
             <p onClick={props.click }>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
