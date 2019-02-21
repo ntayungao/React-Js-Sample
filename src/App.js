78debd31ccbd981425538a5693a9b0779982652d
Niles Kim Ayungao <nileskim.ayungao@gmail.com>
2019-02-21 14:08:02 +0800

    additional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -95,4 +104,4 @@ class App extends Component {
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
 }
 
-export default App;
+export default Radium(App);
