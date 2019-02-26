fe2866b87fb85d48608ee69445eb6ed35316089e
Niles Kim Ayungao <nileskim.ayungao@gmail.com>
2019-02-26 16:17:47 +0800

    Changed

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,6 +23,10 @@ class App extends Component {
     console.log('[App.js] getDerivedStateFromProps', props);
     return state;
   }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount')
+  }
   
   componentDidMount() {
     console.log('[App.js] componentDidMount');
