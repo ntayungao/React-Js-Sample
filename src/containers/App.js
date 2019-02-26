04a193d6dba9519f7561de9fdddcf684f1cd62e3
Niles Kim Ayungao <nileskim.ayungao@gmail.com>
2019-02-26 14:32:19 +0800

    Additional

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Person from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       {name: 'Max', age: 28},
@@ -12,6 +17,15 @@ class App extends Component {
     ],
     otherState: 'Some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+  
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -46,6 +60,7 @@ class App extends Component {
   }
 
   render (){
+    console.log('[App.js] render');
     let persons = null;
     
     if (this.state.showPersons){
