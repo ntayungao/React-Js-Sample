7502731ac0f8dc1b37570e35cd431b1fd6360578
Niles Kim Ayungao <nileskim.ayungao@gmail.com>
2019-02-26 17:02:14 +0800

    Additional

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,6 +3,33 @@ import React, { Component } from 'react';
 import Person from'./Person/Person';
 
 class Persons extends Component {
+    //tatic getDerivedStateFromProps(props, state){
+    //    console.log('[Persons.js] getDerivedStateFromProps');
+    //    return state;
+    //}
+
+    //componentWillReceiveProps(props) {
+    //    console.log('[Persons.js] componentWillReceiveProps', props);
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return {message: 'Snapshot!'};
+    }
+
+    //componentWillUpdate(){
+
+    //}
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('[Persons.js] componentDidUpdate');
+        console.log(snapshot);
+    }
 
     render () {
         console.log('[Persons.js] rendering...');
@@ -13,13 +40,11 @@ class Persons extends Component {
                     name={person.name}
                     age={person.age}
                     key={person.id}
-                    changed={( event ) => this.props.changed( event, person.id)} />
+                    changed={( event ) => this.props.changed( event, person.id)}
+                    />
             );
         });
-
     }
-    
-    
 };
 
 export default Persons;
