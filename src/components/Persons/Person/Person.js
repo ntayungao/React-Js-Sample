6428b9828c5c20cf2bf68f98538380c41a17bc50
Niles Kim Ayungao <nileskim.ayungao@gmail.com>
2019-03-05 14:44:43 +0800

    update

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,10 +12,13 @@ class Person extends Component {
         super(props);
         this.inputElementRef = React.createRef();
     }
+
+    static contextType = AuthContext;
     
     componentDidMount() {
        // this.inputElement.focus();
        this.inputElementRef.current.focus();
+       console.log(this.context.authenticated);
     }
 
     render(){
@@ -23,10 +26,8 @@ class Person extends Component {
 
         return (
             <Aux>
-                <AuthContext.Consumer>
-                    {(context) => context.authenticated ? <p>Authenticated</p> : <p>Please Log in </p>}
-                </AuthContext.Consumer>
-                {}
+        {this.context.authenticated ? (<p>Authenticated</p>) : (<p>Please Log in </p>)}
+
                 <p onClick={this.props.click }>
                 I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
